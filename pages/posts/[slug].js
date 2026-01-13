71480fe1ea437623e2ef8443b117f5ac165aebe1
1159 Essays <homework1078@gmail.com>
2026-01-14 00:17:43 +0100

    Update [slug].js

--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -1,6 +1,7 @@
 import { useRouter } from 'next/router';
+import Link from 'next/link';
 
-export default function PostPage({ post }) {
+export default function PostPage({ post, menuItems }) {
   const router = useRouter();
 
   if (router.isFallback) return <p>Loading...</p>;
@@ -8,6 +9,19 @@ export default function PostPage({ post }) {
 
   return (
     <div style={{ padding: '20px' }}>
+      {/* WordPress menu on top */}
+      <nav style={{ marginBottom: '40px' }}>
+        {menuItems?.map((item) => (
+          <Link
+            key={item.id}
+            href={item.path}
+            style={{ marginRight: '20px', textDecoration: 'none', color: 'blue' }}
+          >
+            {item.label}
+          </Link>
+        ))}
+      </nav>
+
       <h1>{post.title}</h1>
       <div dangerouslySetInnerHTML={{ __html: post.content }} />
     </div>
@@ -49,7 +63,8 @@ export async function getStaticPaths() {
 export async function getStaticProps({ params }) {
   const endpoint = 'http://blog.good-essays.com/graphql';
 
-  const query = `
+  // Query for the single post
+  const postQuery = `
     query ($slug: ID!) {
       post(id: $slug, idType: SLUG) {
         title
@@ -59,21 +74,50 @@ export async function getStaticProps({ params }) {
     }
   `;
 
-  const res = await fetch(endpoint, {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify({
-      query,
-      variables: { slug: params.slug },
-    }),
-  });
+  // Query for the WordPress menu
+  const menuQuery = `
+    query {
+      menu(id: "PRIMARY", idType: NAME) {
+        menuItems {
+          nodes {
+            id
+            label
+            path
+          }
+        }
+      }
+    }
+  `;
 
-  const json = await res.json();
+  try {
+    // Fetch post
+    const postRes = await fetch(endpoint, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        query: postQuery,
+        variables: { slug: params.slug },
+      }),
+    });
+    const postJson = await postRes.json();
+    const post = postJson.data?.post || null;
 
-  return {
-    props: {
-      post: json.data?.post || null,
-    },
-    revalidate: 60,
-  };
+    // Fetch menu
+    const menuRes = await fetch(endpoint, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ query: menuQuery }),
+    });
+    const menuJson = await menuRes.json();
+    const menuItems = menuJson.data?.menu?.menuItems?.nodes || [];
+
+    return {
+      props: { post, menuItems },
+      revalidate: 60,
+    };
+  } catch (error) {
+    console.error('Error fetching post or menu:', error);
+    return { props: { post: null, menuItems: [] } };
+  }
 }
+
